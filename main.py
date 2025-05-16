7623037c3080361d89a67af206738829685f0c88
haiminh <haiminhht2k4@gmail.com>
2025-05-16 15:33:31 +0700

    done

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ BOT_LUX = TelegramBot(
     "7554041257:AAFUAwMCoYs4491t6vDocZ1W9qBvbvjVzto", "BOT_LUX", "https://t.me/TNH9898")
 if __name__ == "__main__":
     while True:
-        tv.Scrap_community_idea(BOT_LUX=BOT_LUX, group_id='-1002406206404')
-        time.sleep(30)
-        c.bot_send_client(BOT_LUX=BOT_LUX, group_id='-1002406206404')
-        time.sleep(120)
+        try:
+            tv.Scrap_community_idea(BOT_LUX=BOT_LUX, group_id='-1002406206404')
+            time.sleep(30)
+            c.bot_send_client(BOT_LUX=BOT_LUX, group_id='-1002406206404')
+            time.sleep(120)
+        except Exception as e:
+            print(f"Error occurred: {e}. Retrying in 60 seconds...")
+            time.sleep(30)
